eb115a0dd6911193d03a956d8970b7fe32817e0b
baskapteijn <baskapteijn@gmail.com>
2018-08-14 22:08:47 +0200

    Initial version of DHT22 sketch

---
+++ sketches/DHT22/DHT22.ino
@@ -0,0 +1,212 @@
+/*
+ * This file is part of the numconvert distribution (https://github.com/baskapteijn/ESP8266Ubidots).
+ * Copyright (c) 2018 Bas Kapteijn.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//TODO: increase send period to 5 minutes
+//TODO: average as many samples as possible per send period
+//TODO: transmit the samples_count and sample_errors as variables too
+
+#include "UbidotsMicroESP8266.h"
+#include <ErriezDHT22.h>
+#include <ErriezTimestamp.h>
+#include <Esp.h>
+#include "private.h"
+
+#define UBIDOTS_UPDATE_INTERVAL_MS  15000 // 15 seconds
+
+// Connect DTH22 DAT pin to Arduino DIGITAL pin
+#if defined(ARDUINO_ESP8266_NODEMCU)
+#define DHT22_PIN      D2
+#else
+#error "May work, but not tested on this target"
+#endif
+
+// Create DHT22 sensor object
+DHT22 sensor = DHT22(DHT22_PIN);
+
+// Create Ubidots client object
+Ubidots client(TOKEN);
+
+// Create timestamp with milliseconds resolution
+TimestampMillis timestamp;
+  
+// Function prototypes
+void printTemperature(int16_t temperature);
+void printHumidity(int16_t humidity);
+
+uint32_t freeMemoryMin = UINT32_MAX;
+uint32_t freeMemoryMax = 0;
+
+static void UpdateFreeMemory(void);
+
+static void UpdateFreeMemory(void)
+{
+  bool updateConsole = false;
+  uint32_t tmpFreeMemory =  ESP.getFreeHeap();
+
+  if (tmpFreeMemory < freeMemoryMin) {
+    freeMemoryMin = tmpFreeMemory;
+    updateConsole = true;
+  }
+
+  if (tmpFreeMemory > freeMemoryMax) {
+    freeMemoryMax = tmpFreeMemory;
+    updateConsole = true;
+  }
+
+  // Only update on change
+  if (updateConsole == true) {
+    Serial.print("Free RAM ");
+    Serial.println(tmpFreeMemory);
+    Serial.print("Free RAM min ");
+    Serial.println(freeMemoryMin);
+    Serial.print("Free RAM max ");
+    Serial.println(freeMemoryMax);
+    Serial.print("\n");
+  }
+}
+
+void setup()
+{
+  // Start timestamp
+  timestamp.start();
+  
+  // Initialize serial port
+  Serial.begin(115200);
+  while (!Serial) {
+      ;
+  }
+  Serial.println("Ubidots DHT22 temperature and humidity example\n");
+
+  // Keep track of free memory and print any changes
+  UpdateFreeMemory();
+  
+  // Initialize sensor
+  sensor.begin();
+
+  // Initialize WIFI connection
+  client.wifiConnection(SSID,PASS); 
+}
+
+unsigned long timestampDelta = 0;
+int16_t temperature = 0;
+int16_t humidity = 0;
+float sendTemperature = 0;
+float sendHumidity = 0;
+
+void loop() {
+  
+  timestampDelta = timestamp.delta();
+
+  // Once every UBIDOTS_UPDATE_INTERVAL_MS
+  if (timestampDelta >= UBIDOTS_UPDATE_INTERVAL_MS) {
+        
+    // Check minimum interval of 2000 ms between sensor reads
+    if (sensor.available()) {
+
+        // Read temperature from sensor (blocking)
+        temperature = sensor.readTemperature();
+
+        // Print temperature (can handle a failed read)
+        printTemperature(temperature);
+
+        // Correct it to 0 on failed reads
+        if (temperature == ~(int16_t)0) {
+          temperature = 0;
+        }
+        
+        // Read humidity from sensor (blocking)
+        humidity = sensor.readHumidity();
+        
+        // Print humidity (can handle a failed read)
+        printHumidity(humidity);
+
+        // Correct it to 0 on failed reads
+        if (humidity == ~(int16_t)0) {
+          humidity = 0;
+        }
+    }
+
+    // Convert the sensor values to floats with a single decimal
+    sendTemperature = (float)temperature / 10;
+    sendHumidity = (float)humidity / 10;
+
+    // Add the variables to the Ubidots data
+    client.add("temperature", sendTemperature);
+    client.add("humidity", sendHumidity);
+
+    // Transmit
+    client.sendAll(true);
+    Serial.println("");
+
+    // Re-start timestamp
+    timestamp.start();
+  }
+
+  // Keep track of free memory and print any changes
+  UpdateFreeMemory();
+}
+
+void printTemperature(int16_t temperature)
+{
+  // Check valid temperature value
+  if (temperature == ~0) {
+    // Temperature error (Check hardware connection)
+    Serial.println("Temperature: Error");
+  } else {
+    // Print temperature
+    Serial.print("Temperature: ");
+    Serial.print(temperature / 10);
+    Serial.print(".");
+    Serial.print(temperature % 10);
+
+    // Print degree Celsius symbols
+    // Choose if (1) for normal or if (0) for extended ASCII degree symbol
+    if (1) {
+        // Print *C characters which are displayed correctly in the serial
+        // terminal of the Arduino IDE
+        Serial.println(F(" *C"));
+    } else {
+        // Note: Extended characters are not supported in the Arduino IDE and
+        // displays ?C. This is displayed correctly with other serial terminals
+        // such as Tera Term.
+        // Degree symbol is ASCII code 248 (decimal).
+        char buf[4];
+        snprintf_P(buf, sizeof(buf), PSTR(" %cC"), 248);
+        Serial.println(buf);
+    }
+  }
+}
+
+void printHumidity(int16_t humidity)
+{
+  // Check valid humidity value
+  if (humidity == ~0) {
+    // Humidity error (Check hardware connection)
+    Serial.println(F("Humidity: Error"));
+  } else {
+    // Print humidity
+    Serial.print(F("Humidity: "));
+    Serial.print(humidity / 10);
+    Serial.print(F("."));
+    Serial.print(humidity % 10);
+    Serial.println(F(" %"));
+  }
+
+  Serial.println();
+}
+
+
---
+++ sketches/DHT22/private.h
@@ -0,0 +1,7 @@
+// WIFI connection
+#define SSID "myssid"
+#define PASS "mypass"
+
+// Ubidots token
+#define TOKEN "mytoken"
+
