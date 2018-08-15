f547c82404d0d2e0ae237105852d7a89951c6db7
baskapteijn <baskapteijn@gmail.com>
2018-08-15 19:07:27 +0200

    Updated private.h with GPLv3 license

--- sketches/DHT22/private.h
+++ sketches/DHT22/private.h
@@ -1,7 +1,30 @@
+/*
+ * This file is part of the ESP8266Ubidots distribution
+ * (https://github.com/baskapteijn/ESP8266Ubidots).
+ * Copyright (c) 2018 Bas Kapteijn.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PRIVATE_H__
+#define PRIVATE_H__
+
 // WIFI connection
 #define SSID "myssid"
 #define PASS "mypass"
 
 // Ubidots token
 #define TOKEN "mytoken"
+
+#endif /* PRIVATE_H__ */
 
